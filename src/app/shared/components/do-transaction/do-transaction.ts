4c9477e1957ff4dd6898b60c402ad6652b31bc9a
Joan <nutsukpuijoan@gmail.com>
2026-01-15 09:33:18 +0100

    add spartant ui component

---
+++ src/app/shared/components/do-transaction/do-transaction.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-do-transaction',
+  imports: [],
+  templateUrl: './do-transaction.html',
+})
+export class DoTransaction {
+
+
+}
